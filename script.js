4b1965f150e6a697c7d34cdc690bde8ac1c02ed0
gerwld <47056812+gerwld@users.noreply.github.com>
2021-05-06 20:57:42 +0200

    Add smooth scroll to anchor

--- script.js
+++ script.js
@@ -29,3 +29,13 @@ function scrollLoop() {
 
     requestAnimationFrame(scrollLoop);
 }
+
+//smooth scroll
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
